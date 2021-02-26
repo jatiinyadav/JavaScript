578369c71c8ceb54ab3b3a02b3021239238507d7
Jatin Yadav <73248007+jatiinyadav@users.noreply.github.com>
2021-02-26 18:49:28 +0530

    update

--- Intermediate/Ex.js
+++ Intermediate/Ex.js
@@ -2,15 +2,19 @@ var output = [];
 var count = 1;
 
 function fizzBuzz() {
-  if (count % 3 == 0 && count % 5 == 0) {
-    output.push("FIZZ BUZZ");
-  } else if (count % 3 == 0) {
-    output.push("FIZZ");
-  } else if (count % 5 == 0) {
-    output.push("BUZZ");
-  } else {
-    output.push(count);
+  while (count <= 10) {
+    if (count % 3 == 0 && count % 5 == 0) {
+      output.push("FIZZ BUZZ");
+    } else if (count % 3 == 0) {
+      output.push("FIZZ");
+    } else if (count % 5 == 0) {
+      output.push("BUZZ");
+    } else {
+      output.push(count);
+    }
+    count++;
+    console.log(output);
   }
-  count++;
-  console.log(output);
 }
+
+fizzBuzz();
