9a46f48892242d8b77c2110b39b3942664e9b0a5
jatiinyadav <jatin27yadav@gmail.com>
2021-05-30 23:23:23 +0530

    update

---
+++ Classes/classes.js
@@ -0,0 +1,59 @@
+//Introduction to Classes
+
+class Dog {
+  constructor(name) {
+    this._name = name;
+    this._behavior = 0;
+  }
+
+  get name() {
+    return this._name;
+  }
+  get behavior() {
+    return this._behavior;
+  }
+
+  incrementBehavior() {
+    this._behavior++;
+  }
+}
+
+const halley = new Dog("Halley");
+console.log(halley.name); // Print name value to console
+console.log(halley.behavior); // Print behavior value to console
+halley.incrementBehavior(); // Add one to behavior
+console.log(halley.name); // Print name value to console
+console.log(halley.behavior); // Print behavior value to console
+
+//--------------------------
+//Constructor
+class Surgeon {
+  constructor(name, department) {
+    this._name = name;
+    this._department = department;
+    this._remainingVacationDays = 20;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get department() {
+    return this._department;
+  }
+
+  get remainingVacationDays() {
+    return this._remainingVacationDays;
+  }
+
+  takeVacationDays(daysOff) {
+    this._remainingVacationDays -= daysOff;
+  }
+}
+
+const surgeonRomero = new Surgeon("Francisco Romero", "Cardiovascular");
+const surgeonJackson = new Surgeon("Ruth Jackson", "Orthopedics");
+
+console.log(surgeonRomero.name);
+surgeonRomero.takeVacationDays(3);
+console.log(surgeonRomero.remainingVacationDays);
---
+++ Objects/objects.js
@@ -0,0 +1,186 @@
+// Write your fasterShip object literal below
+let fasterShip = {
+  color: "silver",
+  "Fuel Type": "Turbo Fuel",
+};
+
+//---------------
+//Accessing Properties
+
+let spaceship = {
+  homePlanet: "Earth",
+  color: "silver",
+  "Fuel Type": "Turbo Fuel",
+  numCrew: 5,
+  flightPath: ["Venus", "Mars", "Saturn"],
+};
+
+// Write your code below
+let crewCount = spaceship.numCrew;
+console.log(crewCount);
+
+let planetArray = spaceship.flightPath;
+console.log(planetArray);
+
+//----------------------
+//Bracket Notation
+
+let spaceship = {
+  "Fuel Type": "Turbo Fuel",
+  "Active Mission": true,
+  homePlanet: "Earth",
+  numCrew: 5,
+};
+
+let propName = "Active Mission";
+
+// Write your code below
+let isActive = spaceship["Active Mission"];
+console.log(spaceship[propName]);
+
+//----------------------
+//Property Assignment
+
+let spaceship = {
+  "Fuel Type": "Turbo Fuel",
+  homePlanet: "Earth",
+  color: "silver",
+  "Secret Mission": "Discover life outside of Earth.",
+};
+
+// Write your code below
+spaceship.color = "glorious gold";
+spaceship["numEngines"] = 4;
+
+delete spaceship["Secret Mission"];
+
+//-----------------------
+//Methods
+
+let retreatMessage =
+  "We no longer wish to conquer your planet. It is full of dogs, which we do not care for.";
+
+// Write your code below
+let alienShip = {
+  retreat() {
+    console.log(retreatMessage);
+  },
+  takeOff() {
+    console.log("Spim... Borp... Glix... Blastoff!");
+  },
+};
+
+alienShip.retreat();
+alienShip.takeOff();
+
+//---------------------
+//Nested Objects
+
+let spaceship = {
+  passengers: [{ name: "Java Script" }],
+  telescope: {
+    yearBuilt: 2018,
+    model: "91031-XLT",
+    focalLength: 2032,
+  },
+  crew: {
+    captain: {
+      name: "Sandra",
+      degree: "Computer Engineering",
+      encourageTeam() {
+        console.log("We got this!");
+      },
+      "favorite foods": ["cookies", "cakes", "candy", "spinach"],
+    },
+  },
+  engine: {
+    model: "Nimbus2000",
+  },
+  nanoelectronics: {
+    computer: {
+      terabytes: 100,
+      monitors: "HD",
+    },
+    "back-up": {
+      battery: "Lithium",
+      terabytes: 50,
+    },
+  },
+};
+
+let capFave = spaceship.crew.captain["favorite foods"][0];
+
+let firstPassenger = spaceship.passengers[0];
+
+//------------------
+//Pass By Reference
+
+let spaceship = {
+  "Fuel Type": "Turbo Fuel",
+  homePlanet: "Earth",
+};
+
+// Write your code below
+
+let greenEnergy = (obj) => {
+  obj["Fuel Type"] = "avocado oil";
+};
+
+let remotelyDisable = (obj) => {
+  obj.disabled = true;
+};
+
+greenEnergy(spaceship);
+
+remotelyDisable(spaceship);
+
+console.log(spaceship);
+
+//-------------------
+//Looping Through Objects
+
+let spaceship = {
+  crew: {
+    captain: {
+      name: "Lily",
+      degree: "Computer Engineering",
+      cheerTeam() {
+        console.log("You got this!");
+      },
+    },
+    "chief officer": {
+      name: "Dan",
+      degree: "Aerospace Engineering",
+      agree() {
+        console.log("I agree, captain!");
+      },
+    },
+    medic: {
+      name: "Clementine",
+      degree: "Physics",
+      announce() {
+        console.log(`Jets on!`);
+      },
+    },
+    translator: {
+      name: "Shauna",
+      degree: "Conservation Science",
+      powerFuel() {
+        console.log("The tank is full!");
+      },
+    },
+  },
+};
+
+// Write your code below
+for (let i in spaceship.crew) {
+  console.log(`${i}: ${spaceship.crew[i].name}`);
+}
+
+console.log("----------------");
+
+for (let j in spaceship.crew) {
+  console.log(`${spaceship.crew[j].name}: ${spaceship.crew[j].degree}`);
+}
+
+//-------------------------
